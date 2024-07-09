b24e2cadf63e218c5613d930218c46d0c174c4d0
sajadaliismail <sajadaliismail@gmail.com>
2024-07-09 23:13:08 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: `http://localhost:3000`, 
+    origin: process.env.ORIGIN, 
     optionsSuccessStatus: 200
   };
 
